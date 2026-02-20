30936c204544c8d56cf03bd08f4449303fd2c65e
Harshini K <harshinikiran.linkedin@gmail.com>
2026-02-20 22:53:29 +0530

    fix start script

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'harshini@123',
-    database: 'ai_student_predictor'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 
 db.connect((err) => {
     if (err) {
