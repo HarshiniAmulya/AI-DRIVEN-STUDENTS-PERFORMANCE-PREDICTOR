8ca444163a063648ca5c6dd91bb8a1fd917ce207
Harshini K <harshinikiran.linkedin@gmail.com>
2026-02-22 14:27:39 +0530

    fixed port for railway

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ cron.schedule("0 8 * * *", () => {
 // ======================
 // START SERVER (IMPORTANT FOR RAILWAY)
 // ======================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
